2122ccce61b0811f03209cb9c966f505e8f2b3ba
adesina13 <dhikrullahabdullahadesina@gmail.com>
2025-06-18 02:48:23 +0100

    task 1

---
+++ quiz.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const feedback = document.getElementById('feedback')
+    const submitAnswer = document.getElementById('submit-answer')
+    function checkAnswer (){
+        let correctAnswer = 4
+        const userAnswer = document.querySelector('input[name="quiz"]:checked').value
+        feedback.style.display = "block"
+        if (userAnswer == correctAnswer){
+            feedback.textContent = 'Correct! Well done.'
+        }else{
+            feedback.textContent = "That's incorrect. Try again!"
+        }
+    }
+
+    submitAnswer.addEventListener('click', checkAnswer)
+
+
+})
